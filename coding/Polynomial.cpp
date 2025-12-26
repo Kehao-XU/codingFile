56b9962671251030d33a98d6cf2886670b5f7bd6
JustinianXU <25303050130@m.fudan.edu.cn>
2025-12-27 00:35:36 +0800

    Halfway there,Polynomial

--- coding/Polynomial.cpp
+++ coding/Polynomial.cpp
@@ -59,7 +59,92 @@ Polynomial create_polynomial(){
     return Poly;
 }
 
-Polynomial add_polynomial(Polynomial p1, Polynomial p2);
-Polynomial multiply_polynomial(Polynomial p1, Polynomial p2);
-void print_polynomial(Polynomial p);
-void destroy_polynomial(Polynomial p);
+Polynomial add_polynomial(Polynomial p1, Polynomial p2){
+    Polynomial SumPoly=(Term*)malloc(sizeof(Term));
+    if(!SumPoly){
+        printf("Memory allocation failed!\n");
+        return NULL;
+    }
+
+    SumPoly->coef=-1;
+    SumPoly->exp=-1;
+    SumPoly->next=NULL;
+
+    Polynomial cur1=p1->next,cur2=p2->next;
+    Polynomial tail=SumPoly,p;
+    while(cur1&&cur2){
+        switch(cur1->exp-cur2->exp){
+            case >0:{}
+            case <0:{}
+            case 0:{}
+        }
+    }
+    
+    while(cur1){
+        p=(Term*)malloc(sizeof(Term));
+        if(!p){
+            printf("Memory allocation failed!\n");
+            destroy_polynomial(SumPoly);
+            return NULL;
+        }
+
+        p->coef=cur1->coef;
+        p->exp=cur1->exp;
+        p->next=NULL;
+
+        tail->next=p;
+        tail=tail->next;
+        cur1=cur1->next;
+    }
+
+    while(cur2){
+        p=(Term*)malloc(sizeof(Term));
+        if(!p){
+            printf("Memory allocation failed!\n");
+            destroy_polynomial(SumPoly);
+            return NULL;
+        }
+
+        p->coef=cur2->coef;
+        p->exp=cur2->exp;
+        p->next=NULL;
+
+        tail->next=p;
+        tail=tail->next;
+        cur2=cur2->next;
+    }
+
+    return SumPoly;
+}
+
+Polynomial multiply_polynomial(Polynomial p1, Polynomial p2){
+    dummy(0);
+}
+
+void print_polynomial(Polynomial p){
+    Polynomial current=p->next;
+    if(!current){
+        printf("This Polynomial doesnt exist!\n");
+        return;
+    }
+
+    while(!current->next){
+        printf("%.2fx^%d+",current->coef,current->exp);
+        current=current->next;
+    }
+
+    if(current->exp==0)printf("%.2f\n",current->coef);
+    else printf("%.2fx^%d\n",current->coef,current->exp);
+
+    return;
+}
+
+void destroy_polynomial(Polynomial p){
+    Polynomial temp=p;
+    while(!p){
+        p=p->next;
+        free(temp);
+        temp=p;
+    }
+    return;
+}
